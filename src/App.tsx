7b57a9d99eff42e264912935c3d64e6dc4256de4
noeliapasaca <napasaca@espol.edu.ec>
2024-06-26 09:39:02 -0500

    Changes

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,50 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WeatherChart from './components/WeatherChart';
+import Grid from '@mui/material/Unstable_Grid2'; // Grid version 2
+import Indicator from './components/Indicator';
+import Summary from './components/Summary';
+import ControlPanel from './components/ControlPanel';
+import BasicTable from './components/BasicTable';
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      <Grid container spacing={5}>
+        <Grid xs={12} sm={4} md={3} lg={2}>1
+          <Grid xs={6} md={4} lg={2}>
+            <Indicator title='Precipitación' subtitle='Probabilidad' value={0.13} />
+          </Grid>
+        </Grid>
+        <Grid xs={6} sm={4} md={3} lg={2}>2
+          <Grid xs={6} sm={4} md={3} lg={2}>
+            <Summary></Summary>
+          </Grid>
+        </Grid>
+        <Grid xs={6} sm={4} md={3} lg={2}>3
+          <Grid xs={12} md={6} lg={9} >
+            <BasicTable></BasicTable>
+          </Grid>
+        </Grid>
+        <Grid xs={12} sm={4} md={3} lg={2}>4
+          <Grid xs={12} lg={2}>
+            <ControlPanel></ControlPanel>
+          </Grid>
+        </Grid>
+        <Grid xs={6} sm={4} md={6} lg={2}>5
+          <Grid xs={12} lg={10}>
+            <WeatherChart></WeatherChart>
+          </Grid>
+        </Grid>
+        <Grid xs={6} sm={4} md={6} lg={2}>6
+
+        </Grid>
+      </Grid>
     </>
   )
 }
